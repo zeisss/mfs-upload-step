ef06944c14d1fe7e159b7a46e7052e070d14b469
Stephan Zeissler <stephan@giantswarm.io>
2016-03-16 20:18:40 +0100

    Fixes quotation

--- run.sh
+++ run.sh
@@ -28,9 +28,9 @@ if [ ! -z "$WERCKER_MFS_UPLOAD_USERNAME" ]; then
 fi
 command="$command ${WERCKER_MFS_UPLOAD_URL}/${WERCKER_MFS_UPLOAD_PATH}"
 
-upload=$(${command} --data-binary @$WERCKER_MFS_UPLOAD_FILE -H"X-ACL: ${WERCKER_MFS_UPLOAD_ACL}" -H'Content-Type: application/octet-stream' --output $WERCKER_REPORT_ARTIFACTS_DIR/mfs_upload_step_response.out -w '%{http_code}')
-if [ "$upload" -eq $WERCKER_MFS_UPLOAD_EXPECTED_RESPONSE ]; then
+upload=$(${command} --data-binary "@$WERCKER_MFS_UPLOAD_FILE" -H"X-ACL: ${WERCKER_MFS_UPLOAD_ACL}" -H'Content-Type: application/octet-stream' --output $WERCKER_REPORT_ARTIFACTS_DIR/mfs_upload_step_response.out -w '%{http_code}')
+if [ "$upload" -eq "$WERCKER_MFS_UPLOAD_EXPECTED_RESPONSE" ]; then
   success "Upload completed."
 else
-  fail "upload: $(cat $WERCKER_REPORT_ARTIFACTS_DIR/mfs_upload_step_response.out)"
+  fail "upload: $(cat "$WERCKER_REPORT_ARTIFACTS_DIR/mfs_upload_step_response.out")"
 fi
